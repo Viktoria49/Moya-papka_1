6e439a8780337132fd7fc9114dcd5ad792aeeba1
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-20 13:25:57 +0700

    задача_2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse (Console.ReadLine());
+
+if (numberA == numberB)
+{
+    Console.WriteLine($"{numberA} = {numberB}");
+}
+else
+if (numberA < numberB)
+{
+    Console.WriteLine($"число {numberB} больше {numberA}");
+}
+else 
+{
+    Console.WriteLine($"{numberA} меньше {numberB}");
+}
