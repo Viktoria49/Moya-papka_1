7bc16f72697d60a8f2192e1258d2c799013c96b7
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-20 14:12:01 +0700

    задача_4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse (Console.ReadLine());
+Console.Write("Введите третее число: ");
+int numberC = int.Parse (Console.ReadLine());
+
+if (numberA == numberB && numberB == numberC)
+{
+    Console.Write("числа равны");
+}
+else{
+if (numberA < numberB && numberB > numberC)
+{
+    Console.Write($"число {numberB} максимальное");
+}
+else
+if (numberB < numberC && numberA < numberC)
+{
+    Console.Write($"число {numberC} максимальное");
+}
+else
+if (numberA > numberC && numberA > numberB);
+{
+    Console.Write($"число {numberA} максимальное");
+}
+}
